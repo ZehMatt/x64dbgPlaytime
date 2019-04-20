6285e2c5e77e46abdef7684713c5dba57837d2eb
Matt <m.moninger.h@gmail.com>
2019-04-20 18:06:51 +0200

    Print out arguments passed to TestApplication

--- src/TestingProject/TestingProject.cpp
+++ src/TestingProject/TestingProject.cpp
@@ -13,7 +13,11 @@ extern "C" __declspec(noinline) void loop_function()
 }
 #pragma optimize("", on)
 
-int main()
+int main(int argc, const char* argv[])
 {
+    for (int i = 0; i < argc; i++)
+    {
+        printf("argv[%d] = %s\n", i, argv[i]);
+    }
     loop_function();
 }
